00445d31811ab5dc4bddcd4d3cbff8d587fe3c7f
Jules Scheil <juliana.m.scheil@gmail.com>
2021-04-20 18:56:07 -0400

    added remove role and add employee

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -3,13 +3,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-id INT,
+id INT AUTO_INCREMENT,
 name VARCHAR(30),
 PRIMARY KEY(ID)
 );
 
 CREATE TABLE role (
-id INT,
+id INT AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
@@ -17,7 +17,7 @@ PRIMARY KEY(id)
 );
 
 CREATE TABLE employee (
-id INT,
+id INT AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
--- employeeTracking.js
+++ employeeTracking.js
@@ -1,14 +1,18 @@
-const inquirer = require('inquirer');
 const mysql = require('mysql');
-
+const inquirer = require('inquirer');
+const util = require("util");
+// create the connection information for the sql database
 const connection = mysql.createConnection({
     host: 'localhost',
+    // Your port; if not 3306
     port: 3306,
+    // Your username
     user: 'root',
+    // Your password
     password: 'Chessie1!',
-    database: 'employee_db'
+    database: 'employee_DB',
 });
-
+connection.promisifiedQuery = util.promisify(connection.query);
 const employeeTrack = () => {
     inquirer
         .prompt({
@@ -17,15 +21,17 @@ const employeeTrack = () => {
             message: 'What would you like to do?',
             choices: [
                 'View all Employees',
-                'View all Employees by Department',
                 'View all Employees by Manager',
                 'Add Employee',
                 'Remove Employee',
-                'Update Employee Role',
+                'Update Role',
                 'Update Employee Manager',
-                'View all Roles',
+                'View Roles',
                 'Add Role',
-                'Remove Role'
+                'Remove Role',
+                'View Departments',
+                'Add Departments',
+                'Remove Departments'
             ]
         })
         .then((answer) => {
@@ -34,41 +40,44 @@ const employeeTrack = () => {
                 case 'View all Employees':
                     viewEmployees();
                     break;
-                case 'View all Employees by Department':
-                    viewByDepartment();
+                    // case 'View all Employees by Manager':
+                    //     viewByManager();
+                    //     break;
+                case 'Add Employee':
+                    addEmployee();
                     break;
-                case 'View all Employees by Manager':
-                    viewByManager();
-                    break;
-                    //     case 'Add Employee':
-                    //         addEmployee();
-                    //         break;
                     //     case 'Remove Employee':
                     //         removeEmployee();
                     //         break;
-                    //     case 'Update Employee Role':
+                    //     case 'Update Role':
                     //         updateRole();
                     //         break;
-                    //     case 'Update Employee Manager':
+                    //     case 'Update Manager':
                     //         updateManager();
-                    //     case 'View all Roles':
+                    //     case 'View Roles':
                     //         viewRoles();
                     //         break;
                     //     case 'Add Role':
                     //         addRole();
                     //         break;
-                    //     case 'Remove Role':
-                    //         removeRole();
-                    //         break;
+                case 'Remove Role':
+                    removeRole();
+                    break;
+                    // case 'View Departments':
+                    //     viewDepartments();
+                    //     break;
+                    // case 'Add Departments':
+                    //     addDepartments();
+                    //     break;
+                    // case 'Remove Departments':
+                    //     removeDepartments();
+                    //     break;
                 default:
                     console.log(`Invalid action: ${answer.action}`);
                     break;
             }
-
         })
 }
-
-
 const viewEmployees = () => {
     connection.query(
         'select a.id, a.first_name, a.last_name, title, department_id, salary, d.concatName from employee_db.employee a left join employee_db.role b on a.role_id=b.id left join employee_db.department c on b.department_id=c.id left join ( select a.first_name, a.last_name, concat(b.first_name,b.last_name) as concatName, a.manager_id,a.id from employee_db.employee a inner join employee_db.employee b on a.manager_id=b.id) d on a.id =d.id',
@@ -77,29 +86,124 @@ const viewEmployees = () => {
             console.table(res);
             employeeTrack();
         })
-
 }
 
-const viewByDepartment = () => {
-    connection.query(
-        'select a.id, a.name, b.first_name, b.last_name, c.title, c.salary, concatName from department a left join role c on a.id = c.department_id left join employee b on c.id = b.role_id left join ( select a.first_name, a.last_name, concat(b.first_name,b.last_name) as concatName, a.manager_id,a.id from employee_db.employee a inner join employee_db.employee b on a.manager_id=b.id) d on a.id =d.id order by a.name',
-        (err, res) => {
-            if (err) throw err;
-            console.table(res);
-            employeeTrack();
-        })
-}
+const addEmployee = () => {
+    const roleArray = await getRoleArray();
+    const managerArray = await getManagerArray();
+    inquirer.prompt([{
+            name: 'first_name',
+            type: 'input',
+            message: 'What is their first name?',
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'What is their last name?',
+        },
+        {
+            name: 'role_id',
+            type: 'list',
+            message: 'What is their role?',
+            choices: roleArray,
+        },
+        {
+            name: 'manager_name',
+            type: "list",
+            message: "What is their manager?",
+            choices: managerArray,
+        }
+    ]).then(({ answer }) => {
+        connection.query(
+            "INSERT INTO employee SET ?", 
+            {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: answer.role_id,
+                manager_id: answer.manager_name
+            },
+            (err, data) => {
+                if (err) throw err;
+                connection.query(
+                    "SELECT * FROM employee WHERE   ?",
+                    [answer],
+                    (err, data) => {
+                        if (err) throw err;
+                        console.log(
+                            `\nYour role is now removed.\n`
+                        );
+                        employeeTrack();
+                    }
+                );
+            }
+        );
+    });
+};
 
-const viewByManager = () => {
-    connection.query(
-        'select concatName, b.first_name, b.last_name, c.title, c.salary from department a left join role c on a.id = c.department_id left join employee b on c.id = b.role_id left join ( select a.first_name, a.last_name, concat(b.first_name,b.last_name) as concatName, a.manager_id,a.id from employee_db.employee a inner join employee_db.employee b on a.manager_id=b.id) d on a.id =d.id order by concatName desc',
-        (err, res) => {
-            if (err) throw err;
-            console.table(res);
-            employeeTrack();
-        })
-}
-
+// const viewByManager = () => {
+//     connection.query(
+//         'select a.id, a.first_name, a.last_name, title, department_id, salary, d.concatName from employee_db.employee a left join employee_db.role b on a.role_id=b.id left join employee_db.department c on b.department_id=c.id left join ( select a.first_name, a.last_name, concat(b.first_name,b.last_name) as concatName, a.manager_id,a.id from employee_db.employee a inner join employee_db.employee b on a.manager_id=b.id) d on a.id =d.id',
+//         (err, res) => {
+//             if (err) throw err;
+//             console.table(res);
+//             employeeTrack();
+//         })
+// }
+const getRoleArray = async () => {
+    const roleArray = await connection.promisifiedQuery(
+        "SELECT * FROM role"
+    );
+    return roleArray.map((role) => {
+        return {
+            name: role.title,
+            value: role.id,
+        };
+    });
+};
+const getManagerArray = async () => {
+    const managerArray = await connection.promisifiedQuery(
+        "select *, concat(b.first_name, b.last_name) as managerName from employee a inner join employee b on a.manager_id = b.id;"
+    );
+    return managerArray.map((manager) => {
+        return {
+            name: manager.manager_name,
+            value: manager.manager_id,
+        };
+    });
+};
+const removeRole = async () => {
+    const roleArray = await getRoleArray();
+    inquirer
+        .prompt([{
+            type: "list",
+            name: "removeRole",
+            choices: roleArray,
+            message: "Which role would you like to remove?",
+        }, ])
+        .then(({
+            removeRole
+        }) => {
+            connection.query(
+                "DELETE FROM role WHERE ?", {
+                    id: removeRole
+                },
+                (err, data) => {
+                    if (err) throw err;
+                    connection.query(
+                        "SELECT title FROM role WHERE title = ?",
+                        [removeRole],
+                        (err, data) => {
+                            if (err) throw err;
+                            console.log(
+                                `\nYour role is now removed.\n`
+                            );
+                            employeeTrack();
+                        }
+                    );
+                }
+            );
+        });
+};
 connection.connect((err) => {
     if (err) throw err;
     employeeTrack();
